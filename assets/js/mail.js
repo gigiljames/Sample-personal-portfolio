417ce0b956d6ebf56052e84cbfe5ec1f44e06aff
Gigil James <70816318+gigiljames@users.noreply.github.com>
2024-08-16 11:45:58 +0530

    email-submission and email-validation completed

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,28 +1,55 @@
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
+document.getElementById("contact-form").addEventListener("submit", sendMail);
+function sendMail(event) {
+  event.preventDefault();
+  const form = document.getElementById("contact-form");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const formData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
 
-    // Initialize EmailJS
-    emailjs.init("K50t6Du4DL8Dtz1y9"); // Replace 'YOUR_USER_ID' with your actual EmailJS User ID
-
-    // Collect form data
-    const formData = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      subject: document.getElementById("subject").value,
-      message: document.getElementById("message").value,
-    };
-
-    alert(formData.email + formData.message + formData.name + formData.subject);
-
-    // Send the email
+  if (
+    formData.name === "" ||
+    formData.message === "" ||
+    formData.email === "" ||
+    formData.subject === ""
+  ) {
+    displayError(form, "Please fill all the fields.");
+  } else if (emailRegex.test(formData.email) == false) {
+    displayError(form, "Please enter a valid email.");
+  } else {
+    // alert(formData.email + formData.message + formData.name + formData.subject);
+    emailjs.init({
+      publicKey: "K50t6Du4DL8Dtz1y9",
+    });
     emailjs.send("service_chbabnu", "template_sbbefu5", formData).then(
       function (response) {
-        alert("Your message has been sent successfully!");
+        // alert("Your message has been sent successfully!");
+        displaySuccess(form, "Your message has been sent successfully!");
       },
       function (error) {
-        alert("Failed to send your message. Please try again later.", error);
+        // alert("Failed to send your message. Please try again later.\n" + error);
+        displayError(
+          form,
+          "Failed to send your message. Please try again later.\n" + error
+        );
       }
     );
-  });
+  }
+}
+
+function displayError(thisForm, error) {
+  thisForm.querySelector(".loading").classList.remove("d-block");
+  thisForm.querySelector(".error-message").innerHTML = error;
+  document.querySelector(".error-message").style.background = "red";
+  thisForm.querySelector(".error-message").classList.add("d-block");
+}
+
+function displaySuccess(thisForm, message) {
+  thisForm.querySelector(".loading").classList.remove("d-block");
+  thisForm.querySelector(".error-message").innerHTML = message;
+  document.querySelector(".error-message").style.background = "green";
+  thisForm.querySelector(".error-message").classList.add("d-block");
+}
